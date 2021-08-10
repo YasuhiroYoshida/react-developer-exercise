db719107500a4e4935e7bf9c8ac87213dc28389b
Yasuhiro Yoshida <892791+YasuhiroYoshida@users.noreply.github.com>
2021-08-10 17:41:09 +0300

    Fix failing App.test.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,8 +1,14 @@
 import { render, screen } from "@testing-library/react";
+import { ApolloProvider } from "@apollo/client";
+import * as graphql from "./graphql";
 import App from "./App";
 
-test("renders", () => {
-  render(<App />);
+test("renders the page title common across pages", () => {
+  render(
+    <ApolloProvider client={graphql.client}>
+      <App />
+    </ApolloProvider>
+  );
   const titleElement = screen.getByText(/GoodGym Developer Exercise/i);
   expect(titleElement).toBeInTheDocument();
 });
